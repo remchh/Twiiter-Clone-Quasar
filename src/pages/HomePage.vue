a902ff70b30c4a2e33bbcb64d619bc59fd9059d6
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2022-04-04 22:02:35 -0600

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page >
+    Home page
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomePage'
+})
+</script>
