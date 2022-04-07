c5d390ea48265421c630c3e5ba38d2774c8f5f69
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2022-04-06 23:21:22 -0600

    new-tweet-form

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,60 @@
 <template>
   <q-page >
-    Home page
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newQtweet"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          autogrow
+          class="new-qweet"
+        >        
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+          </q-avatar>
+        </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          :disable="!newQtweet"
+          class="q-mb-lg"
+          no-caps
+          unelevated
+          rounded
+          color="primary"
+          label="Qtweet" 
+        />
+      </div>
+    </div>
+    <q-separator size="10px" color="grey-2" class="divider"/>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'HomePage'
+  name: 'HomePage',
+  setup(){
+    const newQtweet = ref('')
+    return { newQtweet }
+  }
 })
+
 </script>
+
+<style lang="sass">
+.new-qweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
+
